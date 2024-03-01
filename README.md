ec2c72e337101b675b7c14c0166a4e30e84d2665
Sean P. Kane <spkane00@gmail.com>
2024-03-01 12:25:50 -0800

    read envvar and secret if set.

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ Forked from [golang/example](https://github.com/golang/example)
 
 ## Build
 
-docker build .
+```sh
+docker buildx build --tag outyet:latest . --load
+```
+
+### Run
+
+```sh
+docker run -e envvar=test -e secret=123 -p 8080:8080 outyet:latest
+```
--- main.go
+++ main.go
@@ -26,6 +26,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -34,7 +35,7 @@ import (
 var (
 	httpAddr   = flag.String("http", ":8088", "Listen address")
 	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
-	version    = flag.String("version", "1.7.5", "Go version")
+	version    = flag.String("version", "1.22.0", "Go version")
 )
 
 const baseChangeURL = "https://go.googlesource.com/go/+/"
@@ -69,11 +70,13 @@ var (
 // It serves the user interface (it's an http.Handler)
 // and polls the remote repository for changes.
 type Server struct {
-	version  string
-	url      string
-	hostname string
-	logic    string
-	period   time.Duration
+	version        string
+	url            string
+	hostname       string
+	logic          string
+	period         time.Duration
+	config_message string
+	secret_message string
 
 	mu  sync.RWMutex // protects the yes variable
 	yes bool
@@ -81,7 +84,19 @@ type Server struct {
 
 // NewServer returns an initialized outyet server.
 func NewServer(version, url string, hostname string, period time.Duration, logic string) *Server {
-	s := &Server{version: version, url: url, hostname: hostname, period: period, logic: logic}
+	config_message := "Note: envvar is not set in the environment."
+	envvar, exists := os.LookupEnv("envvar")
+	if exists != false {
+		config_message = fmt.Sprintf("envar is set to: %s", envvar)
+	}
+	secret_message := "Note: secret is not set in the environment."
+	secret, s_exists := os.LookupEnv("secret")
+	if s_exists != false {
+		length := len(secret)
+		redacted_secret := strings.Repeat("*", length)
+		secret_message = fmt.Sprintf("secret is set to: %s", redacted_secret)
+	}
+	s := &Server{version: version, url: url, hostname: hostname, period: period, config_message: config_message, secret_message: secret_message, logic: logic}
 	go s.poll()
 	return s
 }
@@ -123,12 +138,16 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	hitCount.Add(1)
 	s.mu.RLock()
 	data := struct {
-		URL      string
-		Version  string
-		Hostname string
-		Logic    string
-		Yes      bool
+		ConfigMessage string
+		SecretMessage string
+		URL           string
+		Version       string
+		Hostname      string
+		Logic         string
+		Yes           bool
 	}{
+		s.config_message,
+		s.secret_message,
 		s.url,
 		s.version,
 		s.hostname,
@@ -147,7 +166,9 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // tmpl is the HTML template that drives the user interface.
 var tmpl = template.Must(template.New("tmpl").Parse(`
-<!DOCTYPE html><html><body><center>
+<!DOCTYPE html><html>
+	<head></head>
+	<body><center>
 	<h2>Is Go {{.Version}} out yet?</h2>
 	<h1>
 	{{if .Yes}}
@@ -159,5 +180,9 @@ var tmpl = template.Must(template.New("tmpl").Parse(`
 	{{end}}
 	</h1>
 	<p>Hostname: {{.Hostname}}</p>
+	<hr>
+	<p>{{.ConfigMessage}}<br>
+	{{.SecretMessage}}</p>
+	<hr>
 </center></body></html>
 `))
